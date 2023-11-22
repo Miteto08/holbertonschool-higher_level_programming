4e9b300dbef2ff5f915a83ceb3188f6cabbe775a
Miteto08 <lechevalier.j53@gmail.com>
2023-11-22 07:23:57 -0800

    commit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
-
-
-result = islower('a')
-print(result)
-
-result = islower('A')
-print(result)
+    return ord(c) >= 97 and ord(c) <= 122
