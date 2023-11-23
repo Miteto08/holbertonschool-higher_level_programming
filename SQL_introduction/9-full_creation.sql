eb7eed5bb5bf20940de08d45ac375b40efed9098
Miteto08 <lechevalier.j53@gmail.com>
2023-11-23 04:01:25 -0800

    me

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,12 +2,12 @@
 -- because Batch 3 is the best!
 
 CREATE table IF NOT EXISTS second_table(
-	id INT
-	name VARCHAR(256)
+	id INT,
+	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table(id, name, score)
-VALUES(1, "John", 10)
-VALUES(2, "Alex", 3)
-VALUES(3, "Bob", 14)
-VALUES(4, "George", 8);
+INSERT INTO second_table(id, name, score) VALUES
+(1, "John", 10)
+(2, "Alex", 3)
+(3, "Bob", 14)
+(4, "George", 8);
