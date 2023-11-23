430c0ad3b73db3063876292d739b8e7c86a55c34
Miteto08 <lechevalier.j53@gmail.com>
2023-11-23 06:41:52 -0800

    me

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
 
-SELECT score, name FROM second_table ORDER BY score DESC WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
