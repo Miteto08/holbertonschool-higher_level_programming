4e8ed78f6fe27e8a4b1afd958bd7f6e8bdc08ab8
Miteto08 <lechevalier.j53@gmail.com>
2023-11-28 04:06:37 -0800

    me

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,11 @@
 
 
 class Square:
-    def __init__(self, size=None):
+    """this is a commentaire"""
+    def __init__(self, size=0):
         """This is a commentaire"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
-
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
+"""Explain a Class Square"""
+
+
+class Square:
+    """this is a commentaire"""
+    def __init__(self, size=0):
+        """This is a commentaire"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
