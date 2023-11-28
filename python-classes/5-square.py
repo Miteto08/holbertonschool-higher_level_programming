e82da21c049ec447c1725b5a8171a8cfc5ae2f9d
Miteto08 <lechevalier.j53@gmail.com>
2023-11-28 04:48:54 -0800

    me

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
 
-"""Explain a Class Square"""
+"""Explain class square"""
 
 
 class Square:
-    """this is a commentaire"""
+    """Shows a square"""
     def __init__(self, size=0):
-        """This is a commentaire"""
-        self.__size = size
+        """Boot fresh square"""
+        self.size = size
 
     @property
     def size(self):
-        """this is my commentaire"""
+        """Acquire current size of square"""
         return self.__size
 
     @size.setter
-    def size(self, value)
-    """So much commentaire"""
-        if type(size) is not int:
+    def size(self, value):
+        """Calibrate current size of square"""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
-        """this is a new commentaire"""
+        """Restores current area of square"""
         return self.__size ** 2
 
+    def my_print(self):
+        """the new commentaire"""
+        for i in range(self.size):
+            print("#" * self.size)
+
+        if not self.size:
+            print()
