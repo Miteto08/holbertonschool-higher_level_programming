101b6b89cb0ba477a414db5529eaf76247f1321f
Miteto08 <lechevalier.j53@gmail.com>
2023-11-30 02:11:58 -0800

    me

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
+const req = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
+req(starWarsUri, function (error, res, body) {
+  console.log(error || JSON.parse(body).title);
+});
