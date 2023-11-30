3979ec04cd38e7ed63709ee7e5a686c0ebe57918
Miteto08 <lechevalier.j53@gmail.com>
2023-11-30 00:59:44 -0800

    me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,3 +1,12 @@
 #!/usr/bin/node
 
+const fs = require ('fs');
 
+const filepath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filepath, content, 'utf8', (error) => {
+	if(error) {
+		console.error(error);
+	}
+});
