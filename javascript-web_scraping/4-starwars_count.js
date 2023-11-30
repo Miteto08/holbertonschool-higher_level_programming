e2f1d626687a9063342ab7a2082224eccb70fab9
Miteto08 <lechevalier.j53@gmail.com>
2023-11-30 04:40:48 -0800

    me

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,3 +1,14 @@
 #!/usr/bin/node
 
+const req = require('request');
 
+req(process.argv[2], function (error, res, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((i, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? i + 1
+        : i;
+    }, 0));
+  }
+});
