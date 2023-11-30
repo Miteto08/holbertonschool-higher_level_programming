8a13923f1da79ce6988df2acaf8fb06c9aac6396
Miteto08 <lechevalier.j53@gmail.com>
2023-11-30 00:18:50 -0800

    me

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-  if (err) {
-    return console.log(err);
-  }
-  process.stdout.write(data);
-});
+function readFile(filePath) {
+    try {
+        const file = new FileReader();
+        file.readAsText(filePath, "utf-8");
+        file.onload = () => {
+            console.log(file.result);
+        };
+    } catch (error) {
+        console.log(error);
+    }
+}
