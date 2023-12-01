58bbb439c7c8e3303e49d59dd44c59343ccc20eb
Miteto08 <lechevalier.j53@gmail.com>
2023-12-01 00:17:22 -0800

    me

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,3 +1,23 @@
 #!/usr/bin/node
 
+const request = require('request');
 
+request(process.argv[2], function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const completedTask = {};
+    const json = JSON.parse(body);
+
+    for (let i = 0; i < json.length; i++) {
+      if (json[i].completed === true) {
+        if (completedTask[json[i].userId] === undefined) {
+          completedTask[json[i].userId] = 0;
+        }
+        completedTask[json[i].userId]++;
+      }
+    }
+
+    console.log(completedTask);
+  } else {
+    console.error('Error making the request:', error);
+  }
+});
